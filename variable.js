89eec70e31b6eb5a4be9b9f1a661215df3ab6d28
Tayokanch <tayoquadri11@gmail.com>
2024-07-02 11:09:44 +0100

    first commit

---
+++ variable.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//string
+var greetings = "Hello Tayo";
+//number
+var myNumber = 23;
+//boolean
+var isloggedIn = true;
+//Any 
+var hero;
+function getHero() {
+    return 3;
+}
+getHero();
---
+++ variable.ts
@@ -0,0 +1,20 @@
+
+//string
+let greetings : string = "Hello Tayo";
+
+//number
+
+let myNumber: number  = 23
+
+//boolean
+let isloggedIn : boolean = true ;
+
+
+//Any 
+
+let hero : string;
+function getHero (){
+    return "Thor"
+}
+ hero = getHero()
+export {}
